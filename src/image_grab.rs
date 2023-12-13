2a970cf21202180eb30b25bcd7d71908b504af4c
xuxiaocheng <2762267080@qq.com>
2023-12-13 11:28:22 +0800

    fix(behavior): 支持自动创建文件夹

--- src/image_grab.rs
+++ src/image_grab.rs
@@ -1,8 +1,14 @@
+use std::path::Path;
 use anyhow::Result;
 use screenshots::Screen;
+use tokio::fs::create_dir_all;
 use tokio::task::JoinSet;
 
 pub async fn capture(directory: &'static str) -> Result<()> {
+    let path = Path::new(directory);
+    if !path.is_dir() {
+        create_dir_all(path).await?;
+    }
     let mut tasks = JoinSet::new();
     for screen in Screen::all()? {
         tasks.spawn(async move {
